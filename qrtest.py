4798ad8b6761b507148e4773dcb69362ab1c3c7a
flyxt <falemailfal@gmail.com>
2025-08-30 21:31:57 +0700

    Remove smth

--- qrtest.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-import base64
-
-qris_code = "https://example.com/payment?transaction_id=123456"
-
-qris_b64 = base64.urlsafe_b64encode(qris_code.encode()).decode()
-qris_url = f"https://ki-ar-kod.netlify.app/?data={qris_b64}"
-
-print(f"Buka link berikut untuk melihat QRIS:\n{qris_url}")
