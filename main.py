0389c564509a6c76b3583f8026d8bd932ff69c6b
xl-cli <jhankdrat4@gmail.com>
2025-09-06 14:46:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import sys
 from api_request import *
 from ui import *
 from paket_xut import get_package_xut
+from paket_mastif import get_package_mastif
+from paket_family_group import show_company_group_menu
 from my_package import fetch_my_packages
 from paket_custom_family import get_packages_by_family
 from auth_helper import AuthInstance
 
 show_menu = True
+
 def main():
     while True:
         active_user = AuthInstance.get_active_user()
@@ -20,7 +23,7 @@ def main():
            
             show_main_menu(active_user["number"], balance_remaining, balance_expired_at)
             
-            choice = input("Pilih menu: ")
+            choice = input("Pilih menu: ").strip()
             if choice == "1":
                 selected_user_number = show_account_menu()
                 if selected_user_number:
@@ -34,13 +37,27 @@ def main():
             elif choice == "3":
                 # XUT 
                 packages = get_package_xut()
-                
                 show_package_menu(packages)
+                continue
             elif choice == "4":
-                family_code = input("Enter family code (or '99' to cancel): ")
+                # mastif 
+                packages = get_package_mastif()
+                show_package_menu(packages)
+                continue
+            elif choice == "5":
+                # multi operator
+                show_company_group_menu(AuthInstance.api_key, active_user["tokens"])
+                continue
+            elif choice == "6":
+                family_code = input("Enter family code (or '99' to cancel): ").strip()
                 if family_code == "99":
                     continue
                 get_packages_by_family(family_code)
+                continue
+            elif choice == "7":
+                # <<< Tambahan: Ganti Tema >>>
+                change_theme_menu()
+                continue
             elif choice == "99":
                 print("Exiting the application.")
                 sys.exit(0)
@@ -62,4 +79,3 @@ if __name__ == "__main__":
         print("\nExiting the application.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    
